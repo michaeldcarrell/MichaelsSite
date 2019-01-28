a6ba407099cc2a895c493c72580f5e426be93d1e
michaeldcarrell <michaeldcarrell@gmail.com>
2019-01-27 21:47:25 -0600

    Darken background

--- resources/js/script.js
+++ resources/js/script.js
@@ -12,21 +12,6 @@ function MichaelDdFunction() {
     document.getElementById("MichaelDropdown").classList.toggle("show");
 }
 
-// Close the dropdown menu if the user clicks outside of it
-/*window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn-michael')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content-michael");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}*/
-
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn-hanna')) {
 
